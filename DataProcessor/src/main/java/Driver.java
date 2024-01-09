d7430616a2eda14982f033118de24dfbf50bbd33
jeetpa <55351987+jeetpa@users.noreply.github.com>
2024-01-09 17:26:24 +0530

    Add files via upload

--- DataProcessor/src/main/java/Driver.java
+++ DataProcessor/src/main/java/Driver.java
@@ -2,21 +2,39 @@ import com.example.dataprocessor.CsvDataProcessor;
 import com.example.dataprocessor.DataProcessor;
 import com.example.dataprocessor.exceptions.DataProcessingException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Driver {
-    public static void main(String[] args) throws FileNotFoundException, InterruptedException, DataProcessingException {
+    public static void main(String[] args) throws  DataProcessingException {
         List<File> files = new ArrayList<>();
-        files.add(new File("C:\\Users\\ASUS\\Downloads\\MOCK_DATA (1).csv"));
-        files.add(new File("C:\\Users\\ASUS\\Desktop\\MOCK_DATA (1).csv"));
-        DataProcessor processor = new CsvDataProcessor(files, Arrays.asList("report_date", "siteid"), Arrays.asList("imp", "rev"));
-        System.out.println(processor.process());
+
+        BufferedReader reader = null;
+        try {
+            System.out.println(args[0]);
+            reader = new BufferedReader(new FileReader(args[0]));
+            String[] fileNames = reader.readLine().split(",");
+            String[] dimensions = reader.readLine().split(",");
+            String[] metrics = reader.readLine().split(",");
+            for(String fileName : fileNames){
+                files.add(new File(fileName));
+            }
+
+            DataProcessor processor = new CsvDataProcessor(files, Arrays.asList(dimensions),Arrays.asList(metrics));
+            System.out.println(processor.process());
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
+
 }
